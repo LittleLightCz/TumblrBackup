7fcf5eadff0c1b68f534286ebeb507d6f502f3c8
LittleLightCz <antonin.brettsnajdr@gmail.com>
2015-07-25 16:41:09 +0200

    Init

---
+++ TumblrBackup.py
@@ -0,0 +1,78 @@
+import urllib.request
+import xml.etree.ElementTree as ET
+import tumblr_downloader as tumblr
+import concurrent.futures
+import re
+import os
+
+__author__ = 'LittleLight'
+
+'''
+Functions
+'''
+def download_worker(namespace, directory, post, num, posts_count):
+   post_num = num+1
+
+   print("Processing post {0} of {1} ...".format(post_num, posts_count))
+
+   post_url = post.find(".//"+namespace+"loc").text.strip()
+   post_date = post.find(".//"+namespace+"lastmod").text.strip()
+   post_date = post_date.replace("T","_").replace("Z","")
+
+   tumblr.download(os.path.join(directory,"post{0}".format(post_num)),post_url,post_date)
+
+'''
+Configuration
+'''
+
+connections = 5
+
+'''
+Main script run
+'''
+
+print("Enter your blog name:")
+blog = input()
+
+
+directory = blog+"_backup"
+
+print("Creating directory: "+directory+" ...")
+if not os.path.exists(directory):
+   os.makedirs(directory)
+
+url = "http://{0}.tumblr.com/".format(blog)
+print("Your blog URL is: "+url)
+
+sitemap_url = url+"sitemap1.xml"
+
+sitemap_xml = ""
+
+print("Fetching sitemap file: "+sitemap_url+" ...")
+with urllib.request.urlopen(sitemap_url) as response:
+   sitemap_xml = response.read().decode("utf-8")
+
+posts = ET.fromstring(sitemap_xml)
+namespace = re.match(u"({.*})\w+$", posts.tag).group(1)
+
+posts_count = len(posts)
+
+
+#Start downloading
+with concurrent.futures.ThreadPoolExecutor(max_workers=connections) as executor:
+    for post,num in zip(posts,range(posts_count)):
+            executor.submit(namespace, download_worker, directory, post, num, posts_count)
+
+
+
+#Create index
+with open(os.path.join(directory,"index.html"),"w", encoding="utf-8") as f:
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == "post.html":
+                _,file_path = os.path.split(root)
+                file_path = os.path.join(file_path,file)
+                f.write('<iframe style="width: 90%; height: 1000px;" src="{0}">'.format(file_path))
+                f.write('</iframe><br>\n')
+
+print("Backup completed!")
---
+++ tumblr_downloader.py
@@ -0,0 +1,109 @@
+import os
+from bs4 import BeautifulSoup
+import urllib.request
+import urllib.parse as u
+import re
+
+__author__ = 'LittleLight'
+
+def _parse(m):
+    return u.quote(m.group(0))
+
+def download(targetDir, url, date):
+    url = re.sub(r"[^/]+$",_parse,url)
+    with urllib.request.urlopen(url) as response:
+        soup = BeautifulSoup(response.read().decode("utf-8"), "html.parser")
+
+        if not os.path.exists(targetDir):
+            os.makedirs(targetDir)
+
+        _process(soup, targetDir)
+
+def _downloadImage(img, targetDir):
+    url = img["src"]
+    filename = re.match(r".*/([^/]+)$",url).group(1)
+
+    print("Downloading image: "+url)
+    urllib.request.urlretrieve(url, os.path.join(targetDir,filename))
+
+    img["src"] = filename
+
+def _getVideoExtension(type):
+    return "."+re.match(r".*/([^/]+)$", type).group(1)
+
+def _processVideo(soup, targetDir):
+    videos = soup.findAll("video")
+    for video in soup.findAll("video"):
+        source = video.find("source")
+
+        url = source["src"]
+        filename = re.match(r".*/([^/]+)$",url).group(1)
+        filename += _getVideoExtension(source["type"]);
+
+        print("Downloading video: "+url)
+        urllib.request.urlretrieve(url, os.path.join(targetDir,filename))
+
+        source["src"] = filename
+
+def _downloadVideos(soup, targetDir):
+    _downloadIframes(soup, targetDir, _processVideo, "tumblr_video_iframe")
+
+def _downloadImages(post, targetDir):
+    for img in post.findAll("img"):
+        _downloadImage(img, targetDir)
+
+def _processPhotoset(soup, targetDir):
+    links = soup.findAll("a",{"class":"photoset_photo"})
+    for link in links:
+        img = link.find("img")
+        #replace image's src for the bigger one
+        img["src"] = link["href"]
+        _downloadImage(img, targetDir)
+        link["href"] = img["src"]
+
+
+def _downloadPhotoset(post, targetDir):
+    _downloadIframes(post, targetDir, _processPhotoset, "photoset")
+
+def _downloadIframes(post, targetDir, process, className = None):
+
+    if className:
+        frames = post.findAll("iframe", {"class":className})
+    else:
+        frames = post.findAll("iframe")
+
+    for frame, num in zip(frames,range(len(frames))):
+        url = frame["src"]
+        frame_name = "frame{0}.html".format(num)
+
+        with urllib.request.urlopen(url) as response:
+            soup = BeautifulSoup(response.read().decode("utf-8"), "html.parser")
+            process(soup, targetDir)
+
+            with open(os.path.join(targetDir,frame_name),"w", encoding="utf-8") as f:
+                f.write(soup.prettify())
+
+        frame["src"] = frame_name
+
+def _downloadMedia(post, targetDir):
+    _downloadImages(post, targetDir)
+    _downloadPhotoset(post, targetDir)
+
+def _process(soup, targetDir):
+    post = soup.find("section", {"class":"post"})
+
+    if post:
+        _downloadMedia(post, targetDir)
+    else:
+        post = soup
+
+    _downloadVideos(soup, targetDir)
+
+    with open(os.path.join(targetDir,"post.html"), "w", encoding="utf-8") as f:
+        f.write('<html>')
+        f.write('<head>')
+        f.write('<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">')
+        f.write('</head>')
+        f.write(post.prettify())
+        f.write('</html>')
+
