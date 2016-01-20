a5c2c9f460b904491705e76ccaddedad06c25126
LittleLightCz <antonin.brettsnajdr@gmail.com>
2016-01-20 18:51:26 +0100

    When you have more than 500 posts, sitemap will be divided into more parts. After this update the script will get them all :-).

--- TumblrBackup.py
+++ TumblrBackup.py
@@ -3,6 +3,7 @@ import xml.etree.ElementTree as ET
 import downloader as tumblr
 import concurrent.futures
 import re, os, indexer
+import requests as req
 
 __author__ = 'LittleLight'
 
@@ -12,8 +13,6 @@ Functions
 def download_worker(namespace, directory, post, num, posts_count):
     post_num = num+1
 
-    print("Processing post {0} of {1} ...".format(post_num, posts_count))
-
     post_url = post.find(".//"+namespace+"loc").text.strip()
     post_date = post.find(".//"+namespace+"lastmod").text.strip()
     post_date = post_date.replace("T","_").replace("Z","")
@@ -21,10 +20,12 @@ def download_worker(namespace, directory, post, num, posts_count):
     postdir = re.match(r".+/(\d+/.+)$",post_url).group(1).replace("/","_")
     targetdir = os.path.join(directory,postdir)
 
+    print("Processing post into directory: {0}".format(targetdir))
+
     if not os.path.exists(targetdir):
         tumblr.download(targetdir,post_url,post_date)
     else:
-        print("%s exists, skipping ..." % targetdir)
+        print("Skipping %s ... directory already exists!" % targetdir)
 
 '''
 Configuration
@@ -48,27 +49,33 @@ if not os.path.exists(directory):
 url = "http://{0}.tumblr.com/".format(blog)
 print("Your blog URL is: "+url)
 
-sitemap_url = url+"sitemap1.xml"
+resp = req.get(url + "sitemap.xml")
+resp.encoding = "UTF-8"
 
-sitemap_xml = ""
+root_sitemap = ET.fromstring(resp.text)
+sitemap_urls = (tag[0].text for tag in root_sitemap)
+sitemap_urls = [url for url in sitemap_urls if "sitemap-pages.xml" not in url]
 
-print("Fetching sitemap file: "+sitemap_url+" ...")
-with urllib.request.urlopen(sitemap_url) as response:
-   sitemap_xml = response.read().decode("utf-8")
-
-posts = ET.fromstring(sitemap_xml)
-namespace = re.match(r"({.*})\w+$", posts.tag).group(1)
-
-posts_count = len(posts)
-
-
-#Start downloading
 with concurrent.futures.ThreadPoolExecutor(max_workers=connections) as executor:
-    for post,num in zip(posts,range(posts_count)):
-        #redundant check
-        post_url = post.find(".//"+namespace+"loc").text.strip()
-        if post_url != url:
-            executor.submit(download_worker, namespace, directory, post, num, posts_count)
+    for sitemap_url in sitemap_urls:
+
+        print("Fetching sitemap file: "+sitemap_url+" ...")
+        resp = req.get(sitemap_url)
+        resp.encoding = "UTF-8"
+
+        sitemap_xml = resp.text
+
+        posts = ET.fromstring(sitemap_xml)
+        namespace = re.match(r"({.*})\w+$", posts.tag).group(1)
+
+        posts_count = len(posts)
+
+        #Start downloading
+        for post,num in zip(posts,range(posts_count)):
+            #redundant check
+            post_url = post.find(".//"+namespace+"loc").text.strip()
+            if post_url != url:
+                executor.submit(download_worker, namespace, directory, post, num, posts_count)
 
 
 #Create index
